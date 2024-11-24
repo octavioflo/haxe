3544008022984f1e150c1424cf9cebb5d1633567
octavioflo <41705533+octavioflo@users.noreply.github.com>
2024-11-23 19:17:06 -0700

    initial commit - adding requirements spring boot backend

---
+++ requirements/src/test/java/com/haxe/requirements/RequirementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haxe.requirements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequirementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
